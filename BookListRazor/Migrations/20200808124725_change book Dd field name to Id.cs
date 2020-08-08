6ca097696c19a8f7e7bd0991db3b174a125fd574
Ethan Liang <liheng.liang@hotmail.com>
2020-08-08 23:59:29 +1000

    Add project files.

---
+++ BookListRazor/Migrations/20200808124725_change book Dd field name to Id.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class changebookDdfieldnametoId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Book",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Dd",
+                table: "Book");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Book",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Book",
+                table: "Book",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Book",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Book");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Dd",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Book",
+                table: "Book",
+                column: "Dd");
+        }
+    }
+}

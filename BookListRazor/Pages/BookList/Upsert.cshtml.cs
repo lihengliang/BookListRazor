a5a27bf5c656830aff0fbd7b62119478f86f9e53
Ethan Liang <liheng.liang@hotmail.com>
2020-08-09 23:28:49 +1000

    Combine view for edit and create book

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // create book
+                return Page();
+            }
+            // update book
+            Book = await _db.Book.FindAsync(id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToPage();
+            }
+
+            if (Book.Id == 0)
+            {
+                _db.Book.Add(Book);
+            }
+            else
+            {
+                _db.Book.Update(Book);
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
